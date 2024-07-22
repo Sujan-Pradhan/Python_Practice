ead6a472d7116c8614caa4c1fa516faab73de71e
Sujan <pradhansujan730@gmail.com>
2024-07-22 13:05:36 +0545

    Dictionaries, Nesting List and Secret Auction

--- Day9/main.py
+++ Day9/main.py
@@ -1,10 +1,42 @@
-programming_dictionary  = {
-    "Bug": "Ab error",
-    "Loop":"A code again and again",
-    123 : "Number"
+# programming_dictionary  = {
+#     "Bug": "Ab error",
+#     "Loop":"A code again and again",
+#     123 : "Number"
+# }
+
+# # print(programming_dictionary[123])
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+# Nesting
+capitals = {
+    "France": "Paris",
+    "Nepal":"Kathmandu",
+    "Germany": "Berlin",
 }
 
-# print(programming_dictionary[123])
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# Nesting a List in a dictionary
+
+travel_log = {
+    "France":["Paris","Lille","Dijon"],
+    "Germany":["Berlin","Hamburg","Stuttgart"]
+}
+
+# Nesting a dictionary in a dictionary
+travel_log = {
+    "France":{"cities_visted":["Paris","Lille","Dijon"],"total_visits":12},
+    "Germany":{"cities_visited":["Berlin","Hamburg","Stuttgart"],"total_visits":5}
+}
+
+# Nesting dictionary in a list
+travel_log = [
+    {
+        "country":"France",
+        "cities_visted":["Paris","Lille","Dijon"],"total_visits":12
+        },
+    {
+        "country":"Germany",
+        "cities_visited":["Berlin","Hamburg","Stuttgart"],"total_visits":5
+        },
+]
